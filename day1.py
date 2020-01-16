e729f3da427326606a0db31f5b333feaa59daa24
Janam254 <50745457+Janam254@users.noreply.github.com>
2020-01-16 22:37:18 +0300

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,13 @@
+space = ' ' * 20
+print( '%s Kamagambo primary' % space )
+print( '%s Kisii' %space)
+print('''
+
+''')
+print('Dear Dad')
+print()
+print()
+print("i love you")
+print('%s yours faithfully' % space)
+print('''%s "sketch sign"''' % space)
+print("%s Janam Ouko, MD" %space)
